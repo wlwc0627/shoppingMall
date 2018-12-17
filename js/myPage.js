b646e2ac6c3cee2afd861a1d907bc3f153b6b32b
wlwc0627 <33441607+actorWen@users.noreply.github.com>
2018-12-17 18:27:17 +0800

    sixth-page end

--- js/myPage.js
+++ js/myPage.js
@@ -53,12 +53,58 @@ $(function(){
             //第四屏到第五屏的滚动动画
             if(index === 4 && nextIndex === 5){
                 $('.hand-05').animate({'bottom':-20},1000, function(){
-                    $('.t1f-05').animate({'bottom': 65}, 1000, function(){
+                    $('.t1f-05').show().animate({'bottom': 65}, 1000, function(){
                         $('.order-05').animate({'bottom':380},1000, function(){
                             $('.words-05').addClass('words-05-a');
                         })
                     });
                 })
+            }
+
+            // 第5屏幕到第6屏幕过渡
+            if(index == 5 && nextIndex == 6 ) {
+                // 沙发的距离 当前屏幕的高度 减去  box 的 bottom  500
+                    $(".t1f-05").animate({"bottom": -(k - 500), "left": "40%", "width": 65}, 1500, function() {
+                        $(".t1f-05").hide();
+                    });
+
+                    $(".box-06").animate({"left": "38%"}, 1500, function() {
+                         $(this).animate({"bottom": 40}, 500, function() {
+                            $(this).hide();
+
+                            // 行走的过程就是 背景移动的过程
+                            // 背景jqury 里面改变比较麻烦  backgroundPositionX  x坐标 
+                            // 
+                            $(".item6").animate({"backgroundPositionX": "100%"}, 4000, function() {
+                                // 当背景动画执行完毕  boy 大小复原  
+                                $(".boy").animate({"height": 305, "bottom": 116}, 1000, function() {
+                                    $(this).animate({ "right": 500}, 500, function() {
+                                        // 门显示出来 模拟打开门的效果
+                                        $(".door").animate({"opacity": 1},200, function() {
+                                            // 之后girl 显示出来
+                                            $(".girl").show().animate({"right": 350, "height": 306 },500, function() {
+                                                  $(".pop-07").show();
+                                                   $(".next").fadeIn();
+                                            })
+                                        });
+                                    });
+                                });
+                            });
+                            // 光的速度
+                            $(".words-06-a").show().animate({"left": "30%"},2000, "easeOutElastic");
+                            // 
+                            $(".pop-06").show();
+                         });
+                    });
+            }
+
+             // 第6屏幕到第7屏幕过渡
+             if(index === 6 && nextIndex === 7 ) {
+                setTimeout(function() {
+                    $(".star").animate({"width": 120}, 500, function() {
+                        $(".good-07").show();
+                    });
+                }, 2000);
             }
         }
     });
